5575fec0e84ef315e8a95c658c7224eb3ae49d54
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-09-15 19:23:31 +0300

    Guess App | styles are changed

--- guessapp/App.js
+++ guessapp/App.js
@@ -1,10 +1,29 @@
-import { StyleSheet, LogBox } from "react-native";
+import { StyleSheet, LogBox, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 LogBox.ignoreLogs(["Remote debugger"]);
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/Images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgorundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgorundImage:{
+    opacity:0.15
+  }
+});
